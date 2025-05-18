b9d851c8fa9c654469ed47fe5b5e7d9e8f4711e7
Sourny <bsourny@gmail.com>
2025-05-18 17:26:15 +0200

    feat(components): impement bottomNavigation

--- src/app/components/BottomNavigation/index.tsx
+++ src/app/components/BottomNavigation/index.tsx
@@ -1,10 +1,37 @@
 'use client';
-import { BottomNav } from '../../styles';
+
+import { AiOutlineHome } from 'react-icons/ai';
+import { HiOutlineEye } from 'react-icons/hi';
+import { BsChat, BsBasket } from 'react-icons/bs';
+import { BottomNav, NavItems, NavLink, Stack } from '../../styles';
 
 export const BottomNavigation = () => {
 	return (
 		<BottomNav>
-			<p>Bottom Navigation</p>
+			<NavItems>
+				<NavLink href="/">
+					<Stack iscenter>
+						<AiOutlineHome size={20} />
+						<span>Accueil</span>
+					</Stack>
+				</NavLink>
+				<NavLink href="/voyants">
+					<HiOutlineEye size={20} />
+					<span>Voyants</span>
+				</NavLink>
+				<NavLink href="/discussions">
+					<Stack iscenter>
+						<BsChat size={20} />
+						<span>Discussions</span>
+					</Stack>
+				</NavLink>
+				<NavLink href="/boutique">
+					<Stack iscenter>
+						<BsBasket size={20} />
+						<span>Boutique</span>
+					</Stack>
+				</NavLink>
+			</NavItems>
 		</BottomNav>
 	);
 };
