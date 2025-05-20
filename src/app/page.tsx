1723f1ca66b34e85eafd2823db6aa061cd9b4714
Sourny <bsourny@gmail.com>
2025-05-20 21:25:28 +0200

    refactor(components): prevent unnecessary re-renders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,57 +7,68 @@ import { BottomNavigation } from './components/BottomNavigation';
 import { BsBasket } from 'react-icons/bs';
 import { Divider } from './components/ui/Divider';
 import { theme } from './theme';
+import { MenuItemType } from './types';
+import { useMemo } from 'react';
 
 export default function ProfilePage() {
-	const mainMenuItems = [
-		{
-			icon: <FaUser className="icon" />,
-			text: 'Mes informations personnelles',
-		},
-		{ icon: <BsBasket className="icon" />, text: 'Recharger mon compte' },
-		{ icon: <FaTag className="icon" />, text: 'Mes achats' },
-	];
+	const mainMenuItems = useMemo<MenuItemType[]>(
+		() => [
+			{
+				icon: <FaUser className="icon" />,
+				text: 'Mes informations personnelles',
+			},
+			{ icon: <BsBasket className="icon" />, text: 'Recharger mon compte' },
+			{ icon: <FaTag className="icon" />, text: 'Mes achats' },
+		],
+		[],
+	);
 
-	const settingsMenuItems = [
-		{ icon: null, text: 'Support client' },
-		{
-			icon: null,
-			text: 'Préférences de paiement',
-			rightContent: (
-				<div className="right-content">
-					<span style={{ color: 'grey' }}>Paypal</span>
-					<RxChevronRight size={20} style={{ color: 'grey' }} />
-				</div>
-			),
-		},
-		{
-			icon: null,
-			text: 'Préférences de communication',
-		},
-		{
-			icon: null,
-			text: 'Langue',
-			rightContent: (
-				<div className="right-content">
-					<span style={{ color: 'grey' }}>Français</span>
-					<RxChevronRight size={20} color="grey" />
-				</div>
-			),
-		},
-	];
+	const settingsMenuItems = useMemo<MenuItemType[]>(
+		() => [
+			{ icon: null, text: 'Support client' },
+			{
+				icon: null,
+				text: 'Préférences de paiement',
+				rightContent: (
+					<div className="right-content">
+						<span style={{ color: 'grey' }}>Paypal</span>
+						<RxChevronRight size={20} style={{ color: 'grey' }} />
+					</div>
+				),
+			},
+			{
+				icon: null,
+				text: 'Préférences de communication',
+			},
+			{
+				icon: null,
+				text: 'Langue',
+				rightContent: (
+					<div className="right-content">
+						<span style={{ color: 'grey' }}>Français</span>
+						<RxChevronRight size={20} color="grey" />
+					</div>
+				),
+			},
+		],
+		[],
+	);
 
-	const accountActionItems = [
-		{
-			icon: <RxExit className="icon" color="grey" />,
-			text: 'Me déconnecter',
-			isDanger: true,
-		},
-		{
-			icon: <RxTrash className="icon" color="grey" />,
-			text: 'Supprimer mon compte',
-			isDanger: true,
-		},
-	];
+	const accountActionItems = useMemo<MenuItemType[]>(
+		() => [
+			{
+				icon: <RxExit className="icon" color="grey" />,
+				text: 'Me déconnecter',
+				isDanger: true,
+			},
+			{
+				icon: <RxTrash className="icon" color="grey" />,
+				text: 'Supprimer mon compte',
+				isDanger: true,
+			},
+		],
+		[],
+	);
 
 	return (
 		<>
