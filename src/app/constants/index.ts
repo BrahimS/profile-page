bff268050d0105af73e4b3f5b334fe839005ec71
Sourny <bsourny@gmail.com>
2025-05-20 22:25:54 +0200

    refactor(app): add constants

---
+++ src/app/constants/index.ts
@@ -0,0 +1,9 @@
+import { Favorite } from '../types';
+
+export const FAVORITES: Favorite[] = [
+	{ id: 1, name: 'Bella', image: '/images/1.png', status: 'online' },
+	{ id: 2, name: 'Gloria', image: '/images/2.png', status: 'online' },
+	{ id: 3, name: 'Donis', image: '/images/3.png', status: 'busy' },
+	{ id: 4, name: 'Anais', image: '/images/4.png', status: 'busy' },
+	{ id: 5, name: 'Alcee', image: '/images/5.png', status: 'online' },
+];
